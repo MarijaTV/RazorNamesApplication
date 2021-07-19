967c1f9360c9479d6c38e3e7f674a256c1c5f5ab
Marija Timonova-Vilčinskaja <marijatv@gmail.com>
2021-07-19 23:28:29 +0300

    Add project files.

---
+++ RazorNamesApplication/Controllers/NameController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RazorNamesApplication.Models;
+
+namespace RazorNamesApplication.Controllers
+{
+    public class NameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SubmitName(NameModel model)
+        {
+            //Reading
+            string readModels = System.IO.File.ReadAllText("Names.json");
+            List<NameModel> receivedModels = JsonConvert.DeserializeObject<List<NameModel>>(readModels);
+            if (receivedModels == null)
+            {
+                receivedModels = new();
+            }
+
+            //Updating File
+            receivedModels.Add(model);
+            //Saving updates
+            string jsonModels = JsonConvert.SerializeObject(receivedModels);
+
+            System.IO.File.WriteAllText("Names.json", jsonModels);
+            return View("NamesDisplay", receivedModels);
+        }
+    }
+}
